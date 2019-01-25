764115cea0767b68d6e73af9e18b4e6552108208
emma-martin <emmamartinalvarez@gmail.com>
2019-01-25 03:35:55 +0100

    homepage, https modified

--- src/services/service.js
+++ src/services/service.js
@@ -1,4 +1,4 @@
-const URL = "http://hp-api.herokuapp.com/api/characters";
+const URL = "https://hp-api.herokuapp.com/api/characters";
 
 
 const getCharacters = () => fetch(URL).then(response => response.json());
