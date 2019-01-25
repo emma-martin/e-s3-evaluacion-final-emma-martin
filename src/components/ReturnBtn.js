9d0074d2e1323beedd6f9c0f656143505cfd49f3
emma-martin <emmamartinalvarez@gmail.com>
2019-01-25 03:21:59 +0100

    Add styles to ReturnBtn

--- src/components/ReturnBtn.js
+++ src/components/ReturnBtn.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import {Link} from 'react-router-dom';
+import './ReturnBtn.scss';
 
 class ReturnBtn extends Component {
     render() { 
         return (
             <Fragment>
             <Link to="/">
-            <button>volver</button>
+            <button className="btn--home">volver</button>
             </Link>
             </Fragment> 
         );
